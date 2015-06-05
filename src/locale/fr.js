bd980e15970c33cbd6a547b3ce3b8b39b0846987
benjipott <pott.benjamin@gmail.com>
2015-06-05 22:41:54 +0200

    Update fr textes

--- src/locale/fr.js
+++ src/locale/fr.js
@@ -7,7 +7,7 @@ define(function () {
             },
             modal: {
                 preorder: 'Afrostream en illimité pendant 1 an pour 49,99€ au lieu de 84€',
-                charged: 'Profitez des films et series avant première dès 1er septembre 2015.',
+                charged: 'Profitez des films et series en avant première dès le 1er septembre 2015.',
                 footer: 'Abonnez vous maintenant. Vous profitez du service en avant première dès 1er septembre.'
             },
             overlay: {},
